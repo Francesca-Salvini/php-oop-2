1cf060484b6b28ab818deaa2565d8bd72f2ce68a
Francesca Salvini <franci98sp@gmail.com>
2021-05-31 16:02:39 +0200

    completato.

--- Prodotto.php
+++ Prodotto.php
@@ -4,18 +4,21 @@ class Prodotto {
     public $marca;
     public $quantita;
     public $tipologia_prodotto;
+    public $prezzo;
 
-    public function __construct($_marca, $_tipologia_prodotto, $_quantita) {
+    public function __construct($_marca, $_tipologia_prodotto, $_quantita, $_prezzo) {
         $this->marca = $_marca;
         $this->tipologia_prodotto = $_tipologia_prodotto;
         $this->quantita = $_quantita;
+        $this->prezzo = $_prezzo;
     }
 
     public function getInfoData() {
         return [
             'marca' => $this->marca,
             'quantita' => $this->quantita,
-            'tipologia_prodotto' => $this->tipologia_prodotto
+            'tipologia_prodotto' => $this->tipologia_prodotto,
+            'prezzo' => $this->prezzo
         ];
     }
 }
--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php
 
+// Importo i file legati alla classe principale ('Prodotto')
 require_once __DIR__ . '/Cibo.php';
 require_once __DIR__ . '/Sapone.php';
 require_once __DIR__ . '/Cancelleria.php';
 require_once __DIR__ . '/User.php';
 
-$pizza = new Cibo('Buitoni', 'pizza', 3);
-var_dump($pizza->getInfoData());
+// prodotto n.1
+$pizza = new Cibo('Buitoni', 'pizza', 3, 2);
+// var_dump($pizza->getInfoData());
 
-$sapone = new Sapone('Coccolino', 'ammorbidente', 2);
-var_dump($sapone->getInfoData());
+// prodotto n.2
+$sapone = new Sapone('Coccolino', 'ammorbidente', 2, 4);
+// var_dump($sapone->getInfoData());
 
-$matite_colorate = new Cancelleria('Stabilo', 'matite_colorate', 10);
-var_dump($matite_colorate->getInfoData());
+// prodotto n.3
+$matite_colorate = new Cancelleria('Stabilo', 'matite_colorate', 10, 1);
+// var_dump($matite_colorate->getInfoData());
 
+// USER : colui che effettua gli acquisti
 $user = new User('Mario', 'Rossi');
-
+// prodotti scelti dall'user 
 $user->addProduct($sapone);
 $user->addProduct($pizza);
-var_dump($user);
+// var_dump($user);
 ?>
 
+<!--HTML-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,12 +38,14 @@ var_dump($user);
 <body>
 
     <h1>Riepilogo ordine</h1>
+    
 
     <?php foreach($user->getProducts() as $product) { ?>
         <div>
             <ul>
                 <li>Tipologia prodotto: <?php echo $product->tipologia_prodotto ?></li>
                 <li>Marca: <?php echo $product->marca ?></li>
+                <li>Prezzo: <?php echo $product->prezzo . ' euro'; ?></li>
                 <li>Quantità: <?php echo $product->quantita ?></li>
                 
             </ul>
